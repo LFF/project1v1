41aba27c0b7cfb34550e7e64ed167e98939d2d5c
Student <student@Habitat.(none)>
2012-11-30 16:43:21 -0500

    php exercise 2 min

---
+++ phpexercise2minutes.php
@@ -0,0 +1,41 @@
+<!--  PHP code -->
+
+<?php
+
+	print "<br>";
+	
+	$DaysPerWeek = 7;
+	
+	$HoursPerDay = 24;
+	
+	
+	$MinutesPerHour = 60;
+	
+	$MinutesInAWeek = $DaysPerWeek * $HoursPerDay * $MinutesPerHour;
+	
+	$HoursPerDay = 26;
+	
+	$MinutesInAWeekBW = $DaysPerWeek * $HoursPerDay * $MinutesPerHour;
+	
+	
+	$period = ".";
+	
+
+	
+	print "Mumber of Minutes in a week are: ";
+	print $MinutesInAWeek;
+	
+	print "<br>";
+	
+	
+	print "In Bilky Way Mumber of Minutes in a week are: ";
+	print $MinutesInAWeekBW;
+	
+	print "<br>";
+	
+	
+	
+	
+	
+?>
+	
