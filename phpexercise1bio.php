7750c14d9481b3cadfe39ef1bd9f85af352bab0d
Student <student@Habitat.(none)>
2012-11-30 16:30:19 -0500

    php exercise 1 bio

---
+++ phpexercise1bio.php
@@ -0,0 +1,40 @@
+<!--  PHP code -->
+
+<?php
+
+	print "<br>";
+	
+	$firstname = "Luis ";
+	$lastname = "Fonseca";
+	
+	$born = "long time ago";
+	
+	$color = "Blue";
+	$period = ".";
+	
+	
+	
+	print "My Name is ";
+	print $firstname ;
+	print $lastname;
+	print $period;
+	
+	
+	print          "<br>";
+	
+	
+	print "I was born ";
+	print $born;
+	print $period;
+	print "<br>";
+	
+	
+	print "My favorite color is ";
+	print $color;
+	print $period;
+	print "<br>";
+	
+	
+	
+?>
+	
